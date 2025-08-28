399871452c18d62c300ea05efd0f51895e699bde
Aysel SÜNDÜK <119888239+aysel-sunduk@users.noreply.github.com>
2025-08-28 13:39:07 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages altında yayın: https://aysel-sunduk.github.io/Portfolio/
+  // Repo adı: Portfolio
+  base: "/Portfolio/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
